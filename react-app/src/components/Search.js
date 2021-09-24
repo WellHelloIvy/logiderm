9807caea2b1c4427a4a32b0aaacef173d415754a
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-24 12:08:26 -0600

    implemented search product functionality

---
+++ react-app/src/components/Search.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const Search = ({ searchQuery, setSearchQuery, setRenderSearchDropdown}) => {
+    const products = Object.values(useSelector(state => state.products))
+
+    const searchResults = products?.filter(product => product.name.toLowerCase().includes(searchQuery?.toLowerCase()))
+
+    const handleClick = () => {
+        setRenderSearchDropdown(false);
+        setSearchQuery('');
+    }
+
+    return(
+        <div>
+            {searchResults.length ?
+                searchResults.map(product =>
+                    <Link key={product.id} to={`products/${product.id}`} onClick={handleClick}>
+                    {product.name}
+                    </Link>
+                )
+                :
+                    <div>
+                        No results matched your search.
+                    </div>
+            }
+        </div>
+    )
+}
+
+export default Search
