00495585d16208e9ba4613ea11e2c6bafe661a77
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-26 15:06:41 -0600

    rendered splash gif

---
+++ react-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer () {
+    return(
+        <footer>
+            <div>
+                Footer goes here
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- react-app/src/components/SplashPage.js
+++ react-app/src/components/SplashPage.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React from 'react';
+import Footer from './Footer';
 
 function SplashPage() {
     return (
         <>
-            <h1>Website Name</h1>
+            <div id='splash-container'>
+                <div className='gif-container'>
+                    <img id='banner-gif' src='https://media4.giphy.com/media/Hd3EXEZmacCoYtwVZR/giphy.gif?cid=ecf05e47591sypnoq8g6kmbz4q04e44gtzcm6s3mluu0qurl&rid=giphy.gif&ct=g' />
+                </div>
+                <div>
+                    <p>words</p>
+                    <p>scribble</p>
+                </div>
+                <div>
+                    <p> : ) </p>
+                    <a href='https://www.linkedin.com/in/ivy-huynh-43718821b/'><img className='prof-link' src='https://i.imgur.com/8jn6Vm2.png' /></a>
+                    <a href='https://github.com/WellHelloIvy'><img className='prof-link' src='https://i.imgur.com/4LBqWMQ.png'/></a>
+                </div>
+            </div>
+
+            <Footer />
         </>
     )
 }
