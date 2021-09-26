72f3f33d2b886f2166197c45d8495ac6683352f3
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-25 18:57:46 -0600

    off-click functionality for dropdowns, close menu icon

--- react-app/src/components/CategoriesDropdown.js
+++ react-app/src/components/CategoriesDropdown.js
@@ -9,6 +9,7 @@ const CategoriesDropdown = ({ setRenderCategoriesDropdown, categoryEntries }) =>
 
     return ( categoryEntries &&
         <div className='dropdown'>
+            <i onClick={handleClick} className="fas fa-times fa-2x"></i>
             <h2>categories</h2>
             {categoryEntries.map(category=>
                 <Link key={category[0]} to={`/categories/${category[0]}`} onClick={handleClick}>{category[1]}
