1def47d96d60d13b36d6e86c39efed8987f745de
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-29 15:54:12 -0600

    validators

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,7 @@
+import re
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, Email
 from app.models import User
 
 
@@ -16,8 +17,16 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+
+def valid_password (form, field):
+    password = field.data
+    reggie = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$"
+    match = re.fullmatch(reggie, password)
+    if not match:
+        raise ValidationError(f'Password must be at least 6 characters, contain one number, uppercase letter, one lowercase letter and one special character (@$!%*?&)')
+
 class SignUpForm(FlaskForm):
     first_name = StringField('first_name', validators=[DataRequired()])
     last_name = StringField('last_name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), user_exists, Email()])
+    password = StringField('password', validators=[DataRequired(), valid_password])
