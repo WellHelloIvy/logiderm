643cd43cf98f3f21b2971ce5c1f69b91e01d0d8d
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-10-01 12:20:55 -0600

    final commit

---
+++ react-app/src/components/PageNotFound/index.js
@@ -0,0 +1,14 @@
+import './PageNotFound.css'
+
+const PageNotFound = () => {
+    return (
+        <div className='not-found'>
+            <h1>404 Page not Found</h1>
+            <h2>The page you're looking for does not exist</h2>
+        </div>
+
+
+    )
+}
+
+export default PageNotFound
