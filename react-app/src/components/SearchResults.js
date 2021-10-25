64de4099a12d51456ca60e3b8c3f6dfea7f8fab9
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-10-25 16:44:34 -0600

    added search filters componant, rendered categories

--- react-app/src/components/SearchResults.js
+++ react-app/src/components/SearchResults.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
+import SearchFilters from "./SearchFilters";
 
 const SearchResults = () => {
+    const [showFilters, setShowFilters] = useState(false)
     const location = useLocation().search
     const searchQuery = new URLSearchParams(location).get('q')
 
@@ -11,8 +13,16 @@ const SearchResults = () => {
 
     const productResults = products?.filter(product => product.name.toLowerCase().includes(searchQuery?.toLowerCase()));
 
+    const handleFiltersClick = () => {
+        setShowFilters(true)
+    }
+
     return (
         <>
+            <button onClick={handleFiltersClick}>Show Filters</button>
+            {showFilters &&
+                <SearchFilters productResults={productResults}/>
+            }
             <h2>Products that match your search:</h2>
             <div className='results'>
                 {productResults.map(product =>
