99434f025a87c0ab1bdec2b1fa8af42e544e989c
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-10-25 20:14:20 -0600

    implemented brand filter functionality

--- react-app/src/components/SearchResults.js
+++ react-app/src/components/SearchResults.js
@@ -1,31 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
-import SearchFilters from "./SearchFilters";
+
 
 const SearchResults = () => {
-    const [showFilters, setShowFilters] = useState(false)
+    const [brandFilter, setBrandFilter] = useState([]);
     const location = useLocation().search
     const searchQuery = new URLSearchParams(location).get('q')
 
     const products = Object.values(useSelector(state => state.products))
 
+
+
     const productResults = products?.filter(product => product.name.toLowerCase().includes(searchQuery?.toLowerCase()));
 
-    const handleFiltersClick = () => {
-        setShowFilters(true)
+    const [filteredResults, setFilteredResults] = useState([...productResults]);
+
+    useEffect(() => {
+        if (brandFilter.length) {
+            setFilteredResults(filteredResults.filter(product => brandFilter.includes(product.brand.toLowerCase())))
+        } else {
+            setFilteredResults([...productResults])
+        }
+    }, [brandFilter])
+
+
+    const handleBrandClick = (e) => {
+        const brandFilterCopy = [...brandFilter]
+        if (brandFilterCopy.includes(e.target.id.toLowerCase())) {
+            const indexOfBrand = brandFilterCopy.indexOf(e.target.id.toLowerCase());
+            brandFilterCopy.splice(indexOfBrand, 1);
+        } else {
+            brandFilterCopy.push(e.target.id.toLowerCase())
+        }
+        setBrandFilter(brandFilterCopy);
+
+
     }
+
+    const brandNames = new Set(productResults.map(product => product.brand))
+    const brandNamesArr = [...brandNames]
 
     return (
         <>
-            <button onClick={handleFiltersClick}>Show Filters</button>
-            {showFilters &&
-                <SearchFilters productResults={productResults}/>
-            }
+            <div>
+                <b>Categories</b>
+
+                {brandNamesArr.map(brand =>
+                    <>
+                        <input onClick={handleBrandClick} type='checkbox' id={brand} name={`${brand}`} ></input>
+                        <label for={`${brand}`}>{`${brand}`}</label>
+                    </>
+                )}
+            </div>
+
             <h2>Products that match your search:</h2>
             <div className='results'>
-                {productResults.map(product =>
+                {filteredResults.map(product =>
                     <Link key={product.id} to={`/products/${product.id}`}>{`${product.brand} ${product.name}`}</Link>
                 )}
             </div>
