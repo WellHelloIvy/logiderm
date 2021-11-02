8c0649f9bb6e588163952c7ee477bb265ee115da
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-11-02 14:16:35 -0600

    refactoring splash page for different media sizes

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -3,28 +3,28 @@ import './SplashPage.css'
 
 function SplashPage() {
     return (
-        <>
-            <div id='splash-container'>
-                <img id='logo' alt='logiderm logo' src='https://i.imgur.com/m66mLpK.png' />
-                <div className='gif-container'>
+
+        <div id='splash-container'>
+            <img id='logo' alt='logiderm logo' src='https://i.imgur.com/m66mLpK.png' />
+            {/* <div className='gif-container'>
                     <img id='banner-gif' alt='' src='https://i.imgur.com/ovB1AZi.jpg' />
-                </div>
-                <div className='prof-div'>
-                    <a href='https://www.linkedin.com/in/ivy-huynh-43718821b/'>
-                        <i className="fab fa-linkedin fa-3x"></i>
-                    </a>
-                    <a href='https://github.com/WellHelloIvy'>
-                        <i className="fab fa-github-square fa-3x"></i>
-                    </a>
-                    <a href='http://programmatic-app.herokuapp.com'>
-                        <i className="far fa-smile fa-3x"></i>
-                    </a>
-                </div>
-                <div className='dev'>
-                    {/* <b>About the Developer:</b> */}
-                </div>
+                </div> */}
+            <div className='prof-div'>
+                <a href='https://www.linkedin.com/in/ivy-huynh-43718821b/'>
+                    <i className="fab fa-linkedin fa-3x"></i>
+                </a>
+                <a href='https://github.com/WellHelloIvy'>
+                    <i className="fab fa-github-square fa-3x"></i>
+                </a>
+                <a href='http://programmatic-app.herokuapp.com'>
+                    <i className="far fa-smile fa-3x"></i>
+                </a>
             </div>
-        </>
+            <div className='dev'>
+                {/* <b>About the Developer:</b> */}
+            </div>
+        </div>
+
     )
 }
 
